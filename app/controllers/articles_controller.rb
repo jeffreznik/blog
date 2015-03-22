63e2267f212acfa421f3aa9d8efc284072fd32cf
Jeff Reznik <reznik.jeff@gmail.com>
2015-03-21 21:57:18 -0600

    Added authentication, comment deletion. Fixed bug in article form partial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: 'jeff', password: 'asdfasdf', except: [:index, :show]
+
   def index
     @articles = Article.all
   end
